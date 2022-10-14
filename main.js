d7928e50fe787ba02ab38b590875e481664a3ea3
Daniel Viguerias <112416188+Blazter98@users.noreply.github.com>
2022-10-13 23:18:20 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+//service worker
+if('serviceWorker' in navigator){
+    console.log('Puedes usar los service worker en tu navegador');
+    navigator.serviceWorker.register('./sw.js')
+                            .then(res => console.log('serviceWorker cargado correctamente',res))
+                            .catch(err => console.log('serviceWorker no se ha podido usar',err))
+}else{
+    console.log('NO puedes usar los service worker en tu navegador');
+}
+    
